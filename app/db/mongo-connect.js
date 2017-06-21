e3060906a7c4438a4492c1972570831a1bbd6d31
tiven.wang <anypossible@foxmail.com>
2017-06-21 12:55:16 +0800

    cloudfoundry

--- app/db/mongo-connect.js
+++ app/db/mongo-connect.js
@@ -8,6 +8,12 @@ module.exports = function(oAppEnv){
   if(oAppEnv.isLocal === true){
       mongoose.connect('mongodb://localhost:27017/test');
   }else{
+    if(oAppEnv.services.mlab) {
+      // pivotal web services
+      mongoose.connect(oAppEnv.services.mlab[0].credentials.uri);
+    }else if(oAppEnv.services.mongodb) {
+      // cloud foundry @SCP
       mongoose.connect(oAppEnv.services.mongodb[0].credentials.uri);
+    }
   }
 }
